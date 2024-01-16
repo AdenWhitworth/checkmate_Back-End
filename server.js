7cfbb24334d24f1a00ddbb7a475d63308ca7aa0d
AdenWhitworth <aden.whitworth@gmail.com>
2024-01-16 10:15:55 -0800

    created .env for service account

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 var admin = require("firebase-admin");
-var serviceAccount = require("./.firebase/service-account.json");
+
+var serviceAccount = 
+{
+  type: "service_account",
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_id,
+  private_key: process.env.private_key.replace(/\\n/g, "\n"),
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url,
+  universe_domain: process.env.universe_domain
+};
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });//Initialize Firebase Admin
@@ -144,7 +159,7 @@ io.on('connection', (socket) => {
 
       } catch (e){//if room is not created, then pass an error to client
         error = true;
-        message = err;
+        message = e;
 
         callback({error,message});//send callback to client with the error joining the room
       }
